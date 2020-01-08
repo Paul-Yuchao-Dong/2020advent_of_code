6e5400a1fc6b8c3cacebfdcdda2dc3de54cbabe2
paul <paulcynic@gmail.com>
2020-01-08 13:08:24 +0800

    minor fixes

--- 01/solution.R
+++ 01/solution.R
@@ -3,17 +3,17 @@ inputs <- rio::import("./01/input.txt")
 
 # inputs <- c(1969, 100756)
 
-total_fuel <- inputs %>% 
-  {. %/% 3 - 2} %>% 
-  floor()
+inputs <- inputs[,1]
+
+total_fuel <- inputs %/% 3 -2
 
 previous_mass <- total_fuel # initial load
 
 while (any(previous_mass>2)) {
-  next_load <- previous_mass %>% 
-    {. %/% 3 - 2}
+  next_load <- previous_mass %/% 3 -2
   
-  next_load[next_load<0] <- 0
+  # next_load[next_load<0] <- 0
+  next_load <- ifelse(next_load<0,0,next_load)
   
   total_fuel <- cbind(total_fuel, next_load)
   
